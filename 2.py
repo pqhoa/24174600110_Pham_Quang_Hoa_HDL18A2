8fb4665bfee383bfb398791243bab4135e951f41
pqhoa <pqhoa.24174600110@sv.uneti.edu.vn>
2024-10-24 16:33:19 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,3 @@
+x = float(input("nhap gia tri x:"))
+bieu_thuc = (-x + (x**2 + 4)**(1/2)) / (x**4 + 1)* (1/7)
+print(f"gia tri cua F(x) voi {x} la {bieu_thuc}")
---
+++ 8.py
@@ -0,0 +1,4 @@
+import math
+x = float(input("gia tri cua x: "))
+bieu_thuc_f = math.log(4, x) + math.log(x, 2)
+print(f"ket qua can co gia tri cua {x} :{bieu_thuc_f:.2f}")
