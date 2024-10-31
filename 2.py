f7d32f851601490a9b2978515919039f7002f3d6
pqhoa <pqhoa.24174600110@sv.uneti.edu.vn>
2024-10-31 14:36:33 +0700

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,3 +1,27 @@
-x = float(input("nhap gia tri x:"))
-bieu_thuc = (-x + (x**2 + 4)**(1/2)) / (x**4 + 1)* (1/7)
-print(f"gia tri cua F(x) voi {x} la {bieu_thuc}")
+print("Nhap toa do I:")
+x = input("x = ")
+y = input("y = ")
+print("Nhap toa do M:")
+a = input("a = ")
+b = input("b = ")
+print("gia tri r:")
+r = input("r = ")
+
+x = float(x)
+y = float(y)
+a = float(a)
+b = float(b)
+r = float(r)
+
+#xét kc từ r đến đường tròn
+IM = ((x - a)**2 + (y - b)**2)**(1/2)
+
+if IM > r:
+   print("M_k_thuoc_dg_tron")
+elif IM < r:
+   print("M nam trong duong tron")
+else:
+    print("M thuoc dg tron")
+
+
+
