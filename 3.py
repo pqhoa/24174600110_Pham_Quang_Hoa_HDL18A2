c65dcc7f0d162c8f0f0e2b9d94f6a43e4528c643
pqhoa <pqhoa.24174600110@sv.uneti.edu.vn>
2024-10-31 14:38:25 +0700

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,24 @@
+a = float(input("nhap a:"))
+b = float(input("nhap b:"))
+c = float(input("nhap c:"))
+if a > b and a > c:
+   print("max_a")
+elif b > a and b > c:
+    print("max_b")
+else:
+    print("max_c")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
