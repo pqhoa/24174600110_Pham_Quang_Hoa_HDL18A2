d85b07e1a28242849d4be28020b7edd1e3737698
pqhoa <pqhoa.24174600110@sv.uneti.edu.vn>
2024-12-20 11:50:17 +0700

    Add files via upload

--- 5.py
+++ 5.py
@@ -1,5 +1,8 @@
-chu_cai = str(input("chu cai:"))
-if chu_cai == a and e and u and i and o:
-    print("this is nguyen am")
-else: 
-    print("phu am") 
+def f():
+    s = '-- Inside f()'
+    print(s)
+
+
+print('Before calling f()')
+f()
+print('After calling f()')
